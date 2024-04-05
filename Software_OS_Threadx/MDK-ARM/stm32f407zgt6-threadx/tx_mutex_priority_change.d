bbc6f335640f1fd53ba5796a1c1677fdfbdcbdbe
mingliang.sui <sui2054131414@163.com>
2024-04-05 22:39:56 +0800

    pref:删除编译过程中间文件

--- Software_OS_Threadx/MDK-ARM/stm32f407zgt6-threadx/tx_mutex_priority_change.d
+++
@@ -1,9 +0,0 @@
-stm32f407zgt6-threadx/tx_mutex_priority_change.o: \
-  ..\Middlewares\ST\threadx\common\src\tx_mutex_priority_change.c \
-  ..\Middlewares\ST\threadx\common\inc\tx_api.h \
-  ..\Middlewares\ST\threadx\ports\cortex_m4\ac6\inc\tx_port.h \
-  ..\Core\Inc\tx_user.h \
-  D:\Program\ Files\ (x86)\MDK\ARM\ARMCLANG\Bin\..\include\stdlib.h \
-  D:\Program\ Files\ (x86)\MDK\ARM\ARMCLANG\Bin\..\include\string.h \
-  ..\Middlewares\ST\threadx\common\inc\tx_thread.h \
-  ..\Middlewares\ST\threadx\common\inc\tx_mutex.h
--- Software_OS_Threadx/MDK-ARM/stm32f407zgt6-threadx/tx_timer_expiration_process.d
+++
@@ -1,9 +0,0 @@
-stm32f407zgt6-threadx/tx_timer_expiration_process.o: \
-  ..\Middlewares\ST\threadx\common\src\tx_timer_expiration_process.c \
-  ..\Middlewares\ST\threadx\common\inc\tx_api.h \
-  ..\Middlewares\ST\threadx\ports\cortex_m4\ac6\inc\tx_port.h \
-  ..\Core\Inc\tx_user.h \
-  D:\Program\ Files\ (x86)\MDK\ARM\ARMCLANG\Bin\..\include\stdlib.h \
-  D:\Program\ Files\ (x86)\MDK\ARM\ARMCLANG\Bin\..\include\string.h \
-  ..\Middlewares\ST\threadx\common\inc\tx_timer.h \
-  ..\Middlewares\ST\threadx\common\inc\tx_thread.h
